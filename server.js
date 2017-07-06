a9a5849bb87899d3149eca03654d20cb2d46bd29
訾绍飞 <zishaofei221@gmail.com>
2017-07-06 13:51:00 +0800

    feat(server): 获取请求路径

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 let http = require("http");
+let url = require('url');
 
 
 let start = () => {
     http.createServer(function (request, response) {
-        console.log(request);
-        console.log(response);
+        let pathname = url.parse(request.url).pathname;
+        console.log('请求路径是'+pathname);
         response.writeHead(200, { "Content-Type": "text/plain" });
         response.write("H2222");
         response.end();
     }).listen(8888);
 }
-exports.start=start;
+exports.start = start;
